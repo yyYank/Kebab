97d9bdfefcd3074b46add36f06033a81d750d262
yy_yank <yy.yank.dev.blog@gmail.com>
2015-12-23 16:15:54 +0900

    modify test

--- src/test/kotlin/KebabTest.kt
+++ src/test/kotlin/KebabTest.kt
@@ -3,33 +3,50 @@ package kebab.junit4
 import kotlin.properties.Delegates
 import kebab.Browser
 import kebab.Configuration
+import org.junit.After
 import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.runner.RunWith
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
+import java.util.concurrent.TimeUnit
 
 class KebabTest {
     val kebabConfEnv : String by Delegates.notNull()
     val kebabConfScript : String by Delegates.notNull()
+    var config : Configuration by Delegates.notNull<Configuration>()
+    var browser : Browser by Delegates.notNull<Browser>()
+
+    @Before
+    fun setup(){
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver")
+        config = Configuration("http://www.google.co.jp/", ChromeDriver())
+        config.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        browser = Browser(config)
+    }
+
+    @After
+    fun teardown() {
+        config.driver.quit()
+    }
 
 
     @Test
     fun test() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver")
-        val config = Configuration("http://www.google.co.jp/", ChromeDriver())
-        val browser = Browser(config)
         browser.drive("http://www.google.co.jp/", {
-            // TODO これがやりたいんだがなかなか大変
             // 画面が表示されていること
-            // assertEquals(title == 'Hello Kotlin')
-            // テキストボックスに「JGGUG」と入力
-            // Greetボタンを押下
-            // $('form').username = 'JKUG'
-            // val sendButton = $('input', name:'send')
-            // sendButton.click()
+            assertEquals("Google", title)
+            // TODO ここらへん出来てない
+            // find(By.tagName("fugafuga")).getAttribute("hogehoge")
+            // 検索ボタンを押下
+            // val searchButton = find(By.cssSelector(".jsb > center:nth-child(1) > input:nth-child(1)"))
+            // 検索を実行する
+            // searchButton.click()
         })
         browser.quit()
     }
