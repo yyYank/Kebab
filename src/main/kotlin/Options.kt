95a3840480c25bc9b36078a55f13d54227afc4a2
yy_yank <yy.yank.dev.blog@gmail.com>
2016-02-22 23:24:08 +0900

    merge siosio's implementation

---
+++ src/main/kotlin/Options.kt
@@ -0,0 +1,30 @@
+/**
+ * Created by yy_yank on 2016/02/22.
+ */
+package kebab
+
+import org.openqa.selenium.WebDriver
+import java.util.concurrent.TimeUnit
+
+class Options {
+
+    private var timeout:Timeout? = null
+
+    fun timeout(init: Timeout.() -> Unit) {
+        val t = Timeout()
+        t.init()
+        this.timeout = t
+    }
+
+    fun setup(driver: WebDriver) {
+        val manage = driver.manage()
+        timeout?.implicitlyWait?.let {
+            val (timeout, timeUnit) = it
+            manage.timeouts().implicitlyWait(timeout, timeUnit)
+        }
+    }
+}
+
+class Timeout {
+      var implicitlyWait: Pair<Long, TimeUnit>? = null
+}
