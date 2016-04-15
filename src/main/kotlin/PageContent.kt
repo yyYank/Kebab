375e89e88ec5b6ff98719e3b219cd16b1f1e07cd
yy_yank <yy.yank.dev.blog@gmail.com>
2016-04-15 23:10:10 +0900

    fix TODO task(processing)

--- src/main/kotlin/PageContent.kt
+++ src/main/kotlin/PageContent.kt
@@ -4,18 +4,18 @@
 package kebab
 
 import java.util.*
-import kotlin.collections.listOf
 
 
-class PageContentTemplateBuilder(val browser : Browser, val container : PageContentContainer, val navigatorFactory : NavigatorFactory) {
+class PageContentTemplateBuilder(val browser: Browser, val container: PageContentContainer, val navigatorFactory: NavigatorFactory) {
     val templates = HashMap<String, PageContentTemplate>()
+
     companion object {
 
-        fun build(browser : Browser,
-                  container : PageContentContainer,
-                  navigatorFactory : NavigatorFactory,
-                  property : String, startAt : Class<*>,
-                  stopAt : Class<*>  =  Any::class.java) : HashMap<String, PageContentTemplate> {
+        fun build(browser: Browser,
+                  container: PageContentContainer,
+                  navigatorFactory: NavigatorFactory,
+                  property: String, startAt: Class<*>,
+                  stopAt: Class<*> = Any::class.java): HashMap<String, PageContentTemplate> {
 
             if (!stopAt.isAssignableFrom(startAt)) {
                 throw IllegalArgumentException("$startAt is not a subclass of $stopAt")
@@ -45,10 +45,10 @@ class PageContentTemplateBuilder(val browser : Browser, val container : PageCont
             return build(browser, container, navigatorFactory, templatesDefinitions)
         }
 
-        fun build(browser : Browser,
-                  container : PageContentContainer,
-                  navigatorFactory : NavigatorFactory,
-                  templatesDefinitions : List<Any>)  : HashMap<String, PageContentTemplate> {
+        fun build(browser: Browser,
+                  container: PageContentContainer,
+                  navigatorFactory: NavigatorFactory,
+                  templatesDefinitions: List<Any>): HashMap<String, PageContentTemplate> {
             val builder = PageContentTemplateBuilder(browser, container, navigatorFactory)
             //        for (templatesDefinition in templatesDefinitions) {
             //            templatesDefinition.delegate = builder
@@ -59,12 +59,97 @@ class PageContentTemplateBuilder(val browser : Browser, val container : PageCont
     }
 }
 
+// TODO 作成中
 class PageContentTemplate {
-    // TODO 定義
-    //    val browser : Browser
-    //    val owner : PageContentContainer
-    //    val name : String
-    //    val params : String
-    //    val factory : Closure
-    //    val navigatorFactory : NavigatorFactory
+    lateinit var browser: Browser
+    lateinit var owner: PageContentContainer
+    lateinit var name: String
+    lateinit var params: PageContentTemplateParams
+    lateinit var factory: Function<Any>
+    lateinit var navigatorFactory: NavigatorFactory
+    val cache = emptyMap<String, Any>()
+
+    constructor(browser: Browser, owner: PageContentContainer, name: String, params: Map<String, Any>, factory: Function<Any>, navigatorFactory: NavigatorFactory) {
+        this.browser = browser
+        this.owner = owner
+        this.name = name
+//        this.params = PageContentTemplateParams(this, params)
+        this.factory = factory
+        this.navigatorFactory = navigatorFactory
+    }
+
+    override fun toString() = "content template '$name' defined by $owner"
+
+
+    fun getConfig() = browser.config
+
+
+//    fun get(args : Any) = params.cache ? fromCache(*args) : create(*args)
+
+
+//    private create(Object[] args)
+//    {
+//        def createAction = {
+//            def factoryReturn = invokeFactory( * args)
+//            def creation = wrapFactoryReturn(factoryReturn, *args)
+//            if (params.required) {
+//                if (creation instanceof TemplateDerivedPageContent) {
+//                    creation.require()
+//                } else if (creation == null) {
+//                    throw new RequiredPageValueNotPresent(this, *args)
+//                }
+//            }
+//            creation
+//        }
+//
+//        def wait = config.getWaitForParam(params.wait)
+//        if (wait) {
+//            try {
+//                wait.waitFor(createAction)
+//            } catch (WaitTimeoutException e) {
+//                if (params.required) {
+//                    throw e
+//                }
+//                e.lastEvaluationValue
+//            }
+//        } else {
+//            createAction()
+//        }
+//    }
+//
+//    private fromCache(Object[] args)
+//    {
+//        def argsHash = Arrays.deepHashCode(args)
+//        if (!cache.containsKey(argsHash)) {
+//            cache[argsHash] = create(*args)
+//        }
+//        cache[argsHash]
+//    }
+//
+//    private invokeFactory(Object[] args)
+//    {
+//        factory.delegate = createFactoryDelegate(args)
+//        factory.resolveStrategy = Closure.DELEGATE_FIRST
+//        factory(*args)
+//    }
+//
+//    private createFactoryDelegate(Object[] args)
+//    {
+//        new PageContentTemplateFactoryDelegate(this, args)
+//    }
+//
+//    private wrapFactoryReturn(factoryReturn, Object[] args)
+//    {
+//        if (factoryReturn instanceof Module) {
+//            factoryReturn.init(this, args)
+//        }
+//        if (factoryReturn instanceof Navigator) {
+//            new TemplateDerivedPageContent(browser, this, factoryReturn, *args)
+//        } else {
+//            factoryReturn
+//        }
+//    }
+}
+
+class PageContentTemplateParams(val template: PageContentTemplate, val params: PageContentTemplateParams) {
 }
