876670577bfa5537755bc793098acc3c34593ee0
yy_yank <yy.yank.dev.blog@gmail.com>
2017-02-09 22:28:31 +0900

    [WIP] JavaScript support

--- src/main/kotlin/support/alert/DefaultAlertAndConfirmSupport.kt
+++ src/main/kotlin/support/alert/DefaultAlertAndConfirmSupport.kt
@@ -2,6 +2,7 @@ package kebab.support.alert
 
 import js.JavascriptInterface
 import kebab.configuration.Configuration
+import org.openqa.selenium.support.ui.Wait
 
 /**
  * Created by yy_yank on 2016/10/02.
@@ -10,16 +11,76 @@ class DefaultAlertAndConfirmSupport(val javascriptInterfaceFactory: () -> Javasc
 
 
     private val UNKNOWN = -1
+    val installKebabStorageScript =
+            """
+            if (!window.kebab) {
+                window.kebab = {};
+            }
+        """
+    val installDialogStorageScript =
+            """
+            $installKebabStorageScript
+            if (!window.kebab.dialogFunctions) {
+                window.kebab.dialogFunctions = new Array();
+            }
+            if (!window.kebab.dialogMessages) {
+                window.kebab.dialogMessages = new Array();
+            }
+        """
 
 
-    private fun getJavascriptInterface(): JavascriptInterface {
-        val js = javascriptInterfaceFactory()
-        return js
+    val javascriptInterface = javascriptInterfaceFactory()
+
+    private fun captureAlert(actions : ()-> Unit, waitParam: Array<Any>?) {
+//        captureDialog(this.&installAlert, 'alert', actions, config.getWaitForParam(waitParam))
     }
 
 
+    private fun installAlert(js : JavascriptInterface) {
+//        js.exec ("""
+//            $installDialogStorageScript
+//            window.kebab.dialogFunctions.push(window.alert);
+//            window.kebab.dialogMessages.push(null);
+//            window.alert = function(msg) {
+//                window.kebab.dialogMessages.pop();
+//                window.kebab.dialogMessages.push(msg);
+//                return true;
+//            };
+//        """)
+    }
+
+    private fun captureDialog(installer : (js : JavascriptInterface) -> Unit, function : String, actions : () -> Unit, wait : Wait<Any>?) {
+        val js = javascriptInterface
+        installer(js)
+        var actionsError : Throwable? = null
+        try {
+            actions()
+        } catch (e : Throwable) {
+            actionsError = e
+        }
+//        val message = fun () : Wait<Any>?{
+//            try {
+//                return  wait ? wait.waitFor { popLastDialogMessage(js) } : popLastDialogMessage(js)
+//            } finally {
+//                // Need to do this even if actions raised exception
+//                popLastDialogFunctionOnto(js, function)
+//            }
+//        }.invoke()
+        if (actionsError != null) {
+            throw actionsError
+        } else {
+//            return message
+        }
+    }
     override fun <K, V> withAlert(params: Map<K, V>, actions: () -> Unit) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        val message = captureAlert(actions, params.wait)
+//        if (message == null) {
+//            throw AssertionError("no browser alert() was raised")
+//        } else if (message == UNKNOWN) {
+//            true
+//        } else {
+//            message.toString()
+//        }
     }
 
     override fun withNoAlert(actions: () -> Unit) {
